568c0aa76771fb3b135376892e173d7783767880
masd01 <76792171+masd01@users.noreply.github.com>
2023-11-29 17:53:05 +0200

    thermoWatch.py

--- ThermoWatch.py
+++ ThermoWatch.py
@@ -4,15 +4,15 @@ import time
 import utime
 from machine import ADC
 
-#set up thermo sensor
+#set up on board thermo sensor
 sensor_temp = ADC(4) 
 conversion_factor = 3.3 / (65535)
 
-#set up the OLED display
+#set up oled display
 i2c = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1))
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
-#set up the buttons
+#set up buttons
 button1 = machine.Pin(2, machine.Pin.IN, machine.Pin.PULL_UP)
 button2 = machine.Pin(3, machine.Pin.IN, machine.Pin.PULL_UP)
 
@@ -20,14 +20,14 @@ button2 = machine.Pin(3, machine.Pin.IN, machine.Pin.PULL_UP)
 hours = 0
 minutes = 0
 seconds = 0
-#days = 0
-#months = 0
-#years = 0
+days = 0
+months = 0
+years = 2020
 times1 = 0
 
 #function to update the time
 def update_time():
-    global hours, minutes, seconds, days, months, years
+    global hours, minutes, seconds
     seconds += 1
     if seconds == 60:
         seconds = 0
@@ -37,6 +37,17 @@ def update_time():
             hours += 1
             if hours == 24:
                 hours = 0
+
+#function to update date
+def update_date():
+    global years, months, days
+    days += 1
+    if days == 30:
+        days = 0
+        months += 1
+        if months == 12:
+            months = 0
+            years += 1
 
 #main loop
 while True:
@@ -61,27 +72,29 @@ while True:
             oled.show()
             time.sleep(1)
         elif times1 == 3:
+            oled.fill(0)
+            oled.text('Set',0,0)
+            oled.text('Day',0,20)
+            oled.show()
+            time.sleep(1)
+        elif times1 == 4:
+            oled.fill(0)
+            oled.text('Set',0,0)
+            oled.text('Month',0,20)
+            oled.show()
+            utime.sleep(1) 
+        elif times1 == 5:
+            oled.fill(0)
+            oled.text('Set',0,0)
+            oled.text('Year',0,20)
+            oled.show()
+            utime.sleep(1)
+        elif times1 == 6:
             times1 = 0
             oled.fill(0)
             oled.text('masd',46,20) # that's me :)
             oled.show()
             time.sleep(1)
-#        elif times1 == 4:           
-#            oled.fill(0)
-#            oled.text('Set',0,0)     #how to do that?
-#            oled.text('Day',0,20)
-#            oled.show()
-#            time.sleep(1)
-#        elif times1 == 5:
-#            oled.fill(0)
-#            oled.text('Month',0,40)     #and that
-#            oled.show()
-#            utime.sleep(1)
-#        elif times1 == 6:
-#            oled.fill(0)
-#            oled.text('Year',0,40)     #hehe, that too!
-#            oled.show()
-#            utime.sleep(1)
 
     #check if button 2 is pressed
     if not button2.value():
@@ -103,12 +116,24 @@ while True:
             oled.text("{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds), 0, 0)
             oled.show()
             time.sleep(0.1)
-#        elif times1 == 3:
-#            day += 1            
-#            oled.fill(0)
-#            oled.text("{:02d}/{:02d}/{}".format(utime.localtime()[2], utime.localtime()[1], utime.localtime()[0]), 0, 20)
-#            oled.show()
-#            utime.sleep(1)
+        elif times1 == 3:
+            days += 1            
+            oled.fill(0)
+            oled.text("{:02d}/{:02d}/{:04d}".format(days, months, years), 0, 20)
+            oled.show()
+            utime.sleep(0.1)
+        elif times1 == 4:
+            months += 1            
+            oled.fill(0)
+            oled.text("{:02d}/{:02d}/{:04d}".format(days, months, years), 0, 20)
+            oled.show()
+            utime.sleep(0.1)
+        elif times1 == 5:
+            years += 1            
+            oled.fill(0)
+            oled.text("{:02d}/{:02d}/{:04d}".format(days, months, years), 0, 20)
+            oled.show()
+            utime.sleep(0.1)
 
     #update time
     update_time()
@@ -120,7 +145,7 @@ while True:
     oled.text("{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds), 0, 0)
 
     #display date
-    oled.text("{:02d}/{:02d}/{}".format(utime.localtime()[2], utime.localtime()[1], utime.localtime()[0]), 0, 20)
+    oled.text("{:02d}/{:02d}/{:04d}".format(days, months, years), 0, 20)
     
     #display temperature
     oled.text('Temp: '+ tstring,0,40)
@@ -130,4 +155,4 @@ while True:
 
     #delay 1 sec
     utime.sleep(1)
-    
+   
